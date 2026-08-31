294599fc9f47595f7059387fba9fdefeb193594a
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-08-31 21:58:40 +0530

    Create shipment.py

---
+++ backend/app/models/shipment.py
@@ -0,0 +1,60 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Float,
+    ForeignKey,
+    DateTime
+)
+
+from sqlalchemy.sql import func
+
+from ..database import Base
+
+
+class Shipment(Base):
+
+    __tablename__ = "shipments"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    shipment_code = Column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+
+    product = Column(
+        String(150),
+        nullable=False
+    )
+
+    supplier_id = Column(
+        Integer,
+        ForeignKey("suppliers.id")
+    )
+
+    quantity = Column(
+        Integer,
+        nullable=False
+    )
+
+    historical_lead_time = Column(Float)
+
+    current_lead_time = Column(Float)
+
+    inventory_level = Column(Float)
+
+    status = Column(
+        String(50),
+        default="Pending"
+    )
+
+    created_at = Column(
+        DateTime,
+        server_default=func.now()
+    )
