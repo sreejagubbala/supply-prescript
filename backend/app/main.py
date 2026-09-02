188da1190df6e0c75f2ad6b23d2d16c69f614bb3
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-09-02 17:08:22 +0530

    Create main.py

---
+++ backend/app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+from .routes import shipments
+
+
+app = FastAPI(
+    title="SupplyPrescript API",
+    description="Backend API for SupplyPrescript",
+    version="1.0.0"
+)
+
+
+app.include_router(
+    shipments.router
+)
+
+
+@app.get("/")
+def root():
+
+    return {
+        "message": "SupplyPrescript Backend is running"
+    }
+
+
+@app.get("/health")
+def health():
+
+    return {
+        "status": "healthy"
+    }
