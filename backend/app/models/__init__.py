eba60873bca6143a9a5afc9566ae4a9da41a0e79
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-09-03 17:30:26 +0530

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,6 @@
 from .shipment import Shipment
 from .supplier import Supplier
+from .prediction import Prediction
+from .prescription import Prescription
 from .decision import Decision
 from .outcome import Outcome
