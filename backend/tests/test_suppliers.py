4e89e7bdb2ee7ebc73e30373b9b131d8ac1eaff0
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2026-09-20 10:07:28 +0530

    Create test_suppliers.py

---
+++ backend/tests/test_suppliers.py
@@ -0,0 +1,22 @@
+from backend.app.main import app
+
+
+def test_supplier_list_api_exists():
+
+    routes = [
+        route.path
+        for route in app.routes
+    ]
+
+    assert "/api/suppliers/" in routes
+
+
+def test_supplier_detail_api_exists():
+
+    routes = [
+        route.path
+        for route in app.routes
+    ]
+
+    assert "/api/suppliers/{supplier_id}" in routes
+  
